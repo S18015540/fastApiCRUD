48f407a85394ddf5c2de2f9246c2c3b18c98685a
Angel Eslava AKA Fallen Knight <angelalbertoeslava@gmail.com>
2023-01-08 16:29:09 -0600

    Inicio

---
+++ Routes/user.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+
+user = APIRouter ()
+
+@user.get("/users")
+def helloworld():
+    return "hello world 2"
+
+@user.get("/users")
+def helloworld():
+    return "hello world 2"
+
+@user.get("/users")
+def helloworld():
+    return "hello world 2"
+
+@user.get("/users")
+def helloworld():
+    return "hello world 2"
+
+@user.get("/users")
+def helloworld():
+    return "hello world 2"
--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from fastapi import FastAPI
-
+from Routes.user import user
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(user)
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
