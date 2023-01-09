45cd513c9c449171dac51db797a2f3c06c749a89
Angel Eslava AKA Fallen Knight <angelalbertoeslava@gmail.com>
2023-01-08 18:36:25 -0600

    Final

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from Routes.user import user
-app = FastAPI()
+app = FastAPI(
+    title= "Prueba CRUD",
+    description= "CRUD FASTAPI",
+    openapi_tags=[{
+        "name": "users",
+        "descripcion": "users Route"
+    }
+    ]
+
+)
 
 app.include_router(user)
 
